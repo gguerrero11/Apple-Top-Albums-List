22fdec957b494baf5677c2a0215b9ce0f24b6e8f
Gabe Guerrero <gguerrero11@gmail.com>
2020-04-20 18:33:31 -0600

    implement navigation bar and albumManager

---
+++ basicAlbum/AlbumTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AlbumTableViewCell.swift
+//  basicAlbum
+//
+//  Created by Gabe Guerrero on 4/20/20.
+//  Copyright © 2020 Gabriel Guerrero. All rights reserved.
+//
+
+import UIKit
+
+class AlbumTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- basicAlbum/AlbumViewController.swift
+++ basicAlbum/AlbumViewController.swift
@@ -11,22 +11,41 @@ import UIKit
 class AlbumViewController: UIViewController {
     @UsesAutoLayout
     var tableView = UITableView()
+    @UsesAutoLayout
+    var navBar = UINavigationBar()
+    
+    let dequeueCellID = "albumCell"
+    let albumManager = AlbumManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        albumManager.albumDownloadCallback = {
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
+        view.backgroundColor = .white
         setupTableView()
+        setupNavigationBar()
     }
     
     func setupTableView() {
         tableView.dataSource = self
         tableView.delegate = self
         tableView.backgroundColor = .white
+        tableView.register(AlbumTableViewCell.self, forCellReuseIdentifier: dequeueCellID)
         view.addSubview(tableView)
         NSLayoutConstraint.activate(tableView.constraintsForAnchoringTo(safeAreaLayoutBoundsOf: view))
+    }
+    
+    func setupNavigationBar() {
+        view.addSubview(navBar)
+        navBar.barStyle = .default
+        NSLayoutConstraint.activate(navBar.constraintsForAnchoringTo(safeAreaLayoutBoundsOf: view))
+        NSLayoutConstraint.activate(navBar.constraintsForHeight(height: 60))
     }
 }
 
@@ -38,12 +57,14 @@ extension AlbumViewController: UITableViewDelegate {
 
 extension AlbumViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return albumManager.albums.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: dequeueCellID) as? AlbumTableViewCell {
+            let album = albumManager.albums[indexPath.row]
+
+        }
         return UITableViewCell()
     }
-    
-    
 }
